f9e4b9ccdfc9b8759c68b78d40b58c6e73c4b9a6
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2022-12-08 14:57:22 +0100

    testing deployment on next

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'font-awesome/css/font-awesome.css'
+import Head from 'next/head';
+import "bootstrap/dist/css/bootstrap.css";
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Navbar/>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
