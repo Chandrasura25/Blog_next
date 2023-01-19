3520ac361ba21510efd3ab01e6d97db5813bd418
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-01-19 14:38:17 +0100

    creating a blog

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,17 @@ import '../styles/globals.css';
 import 'font-awesome/css/font-awesome.css'
 import Head from 'next/head';
 import "bootstrap/dist/css/bootstrap.css";
-import Navbar from '../components/Navbar';
+import '@mui/material'
+// import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
       </Head>
-      <Navbar/>
       <Component {...pageProps} />
     </>
   );
