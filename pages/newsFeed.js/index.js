5a825e61de31d57ebdf53afb0065be44356cfc57
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-01-25 17:08:14 +0100

    on news feed

--- pages/newsFeed.js/index.js
+++ pages/newsFeed.js/index.js
@@ -5,10 +5,13 @@ import Head from 'next/head'
 
 const NewsFeed = () => {
   return <>
+    <Head>
+      <title>News Feed</title>
+    </Head>
     <div classname='container-fluid'>
       <div className='col-md-7'>
         <div>
-          <Image src={img} alt='img' />
+          {/* <Image src={img} alt='img' /> */} 
         </div>
       </div>
       <div className='col-md-3'></div>
