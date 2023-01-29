c9057f11a3f0bb11d92c5f104d38bc1fb2c8d61b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-01-29 05:28:37 +0100

    nav menu added

--- components/Indicator.js
+++ components/Indicator.js
@@ -1,56 +1,25 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import styles from '../styles/Indicator.module.css'
 const Indicator = () => {
-    function activeLink() {
-        const list = document.querySelectorAll('.list');
-        list.forEach((item) =>{item.classList.remove('active')});
-        this.classList.add('active')
-    }
-    // const list = document.querySelectorAll('.list');
-    // list.forEach((item) =>
-    //     item.addEventListener('click', activeLink));
+    const navigation = useRef();
+    const toggle = useRef();
+    const toggling=()=>{
+            toggle.current.classList.toggle('active')
+            navigation.current.classList.toggle('active')
+        }
     return (
         <>
-            <div className={styles.navigation}>
+            <div className={styles.navigation} ref={navigation} id='navigate'>
+                <div className={styles.toggle} onClick={toggling} ref={toggle} id='togg'><span></span></div>
                 <ul>
-                    <li className={styles.active} style={{ "--clr": "#f44336" }}>
-                        <a href="#">
-                            <span className={styles.icon}>
-                                <i className="fa fa-home" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </li>
-                    <li className={styles.list} style={{ "--clr": "#ffa117" }}>
-                        <a href="#">
-                            <span className={styles.icon}>
-                                <i className="fa fa-user" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </li>
-                    <li className={styles.list} style={{ "--clr": "#0fc70f" }}>
-                        <a href="#">
-                            <span className={styles.icon}>
-                                <i className="fa fa-crosshairs" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </li>
-                    <li className={styles.list} style={{ "--clr": "#2196f3" }}>
-                        <a href="#">
-                            <span className={styles.icon}>
-                                <i className="fa fa-camera" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </li >
-                    <li className={styles.list} style={{ "--clr": "#b145e9" }}>
-                        <a href="#">
-                            <span className={styles.icon}>
-                                <i className="fa fa-cog" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </li >
-                    <div className={styles.indicator}></div>
-                </ul >
-            </div >
+                    <li style={{"--i":"0"}}><a href="#">Home</a></li>
+                    <li style={{"--i":"1"}}><a href="#">About</a></li>
+                    <li style={{"--i":"2"}}><a href="#">Services</a></li>
+                    <li style={{"--i":"3"}}><a href="#">Work</a></li>
+                    <li style={{"--i":"4"}}><a href="#">Team</a></li>
+                    <li style={{"--i":"5"}}><a href="#">Contact</a></li>
+                </ul>
+            </div>
         </>
     )
 }
